39492fbbc45625cffe30679851397e85c49ed754
Ahmad Osman <ahmad@ahmad-osman.com>
2021-02-26 17:33:17 +0100

    final 1

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-$(document).ready(function(){
-    $('.customer-logos').slick({
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 3
-            }
-        }]
-    });
-});
+// $(document).ready(function(){
+//     $('.customer-logos').slick({
+//         slidesToShow: 6,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 1500,
+//         arrows: false,
+//         dots: false,
+//         pauseOnHover: false,
+//         responsive: [{
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 4
+//             }
+//         }, {
+//             breakpoint: 520,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         }]
+//     });
+// });
 
-function readMore() {
-    const dots = document.getElementById("dots");
-    const moreText = document.getElementById("more");
-    const btnText = document.getElementById("myBtn");
+// function readMore() {
+//     const dots = document.getElementById("dots");
+//     const moreText = document.getElementById("more");
+//     const btnText = document.getElementById("myBtn");
   
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
-  }
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read more";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read less";
+//       moreText.style.display = "inline";
+//     }
+//   }
