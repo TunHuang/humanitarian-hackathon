86db01d88910d61a11450a39321519e834d3c1da
TunHuang <huang.tun.anton@gmail.com>
2021-02-26 11:47:37 +0100

    added blog section

--- js/script.js
+++ js/script.js
@@ -20,3 +20,19 @@ $(document).ready(function(){
         }]
     });
 });
+
+function readMore() {
+    const dots = document.getElementById("dots");
+    const moreText = document.getElementById("more");
+    const btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
